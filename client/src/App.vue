7b43c2926c03c54d16783893533aa4238aae0a87
Richard Bodsworth <richardbodsworth@gmail.com>
2021-01-20 11:07:56 +0000

    server running

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+name: 'app',
+
+  mounted(){
+    fetch("http://localhost:3000/")
+      .then(response => response.json())
+      .then(data => this.serverMessage = data.message);
+},
+ data () {
+    return {
+      serverMessage: []
+    }
   }
 }
+
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
