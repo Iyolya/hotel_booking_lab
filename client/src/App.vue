a1acb873e9dbe4741ae6f2080b0efbdb1b5c4f1e
Richard Bodsworth <richardbodsworth@gmail.com>
2021-01-20 14:39:13 +0000

    booking list

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,27 +2,30 @@
 <div>
   <h1>Overlook Hotel</h1>
 <booking-form></booking-form>
+<booking-list></booking-list>
 </div>
 </template>
 
 <script>
 
 import BookingForm from './components/BookingForm.vue';
+import BookingList from './components/BookingList.vue';
 
 export default {
 name: 'app',
 components: {
-  'booking-form': BookingForm
+  'booking-form': BookingForm,
+  'booking-list': BookingList
 },
 
   mounted(){
     fetch("http://localhost:3000/")
       .then(response => response.json())
-      .then(data => this.serverMessage = data.message);
+      .then(data => this.bookings = data.message);
 },
  data () {
     return {
-      serverMessage: []
+      bookings: []
     }
   }
 }
---
+++ client/src/components/BookingList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="(booking, index) in bookings" :key="index" :bookings="bookings">
+            Name {{booking.name}}
+            {{booking.email}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+
+import BookingService from '../services/BookingService.js';
+import {eventBus} from '@/main.js'
+
+export default {
+    name: 'booking-list',
+    props: ['bookings'],
+    data: function() {
+        return {
+            bookings: []
+        }
+    },
+
+    mounted: function() {
+        BookingService.getBookings()
+        .then(bookings => this.bookings = bookings)
+
+        eventBus.$on('booking-completed', booking => this.bookings.push(booking))
+    }
+
+    
+
+}
+    
+
+</script>
+
+<style>
+
+</style>
